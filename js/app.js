6a562470f63cd36f9e63d63056cd0b1fa9c19586
KellerC21 <115097254+KellerC21@users.noreply.github.com>
2023-05-05 08:42:53 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -2,3 +2,27 @@ baguetteBox.run('.gallery');
 
 const search = new Filter('search', 'data-caption');
 
+document.queryselector('#search').addEventListener('keyiup', userSearch);
+
+function userSearch() {
+    const caption = document.querySelectorAll ('a[data-caption]');
+    let captionsList = [];
+
+    for (let i=0; i<caption.length; i++){
+        let captions = caption[i].getAttribute('data-caption');
+        captionsLet.push(captions.toLowerCase());
+
+        let searchVar = document.querySelector('#search');
+        searchVar = searchVar.ariaValueMax.toLowerCase();
+
+    if(captionsList[i].indexOf(searchVar)>-1){
+        captions[i].style.display='block';
+    }
+    else{
+        caption[i].style.display='none';
+
+        }
+    }
+}
+
+console.log(userSearch());
