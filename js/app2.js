64cdcf92c6f9df2400cda0b5b4ac50d9441d565d
KellerC21 <115097254+KellerC21@users.noreply.github.com>
2023-05-08 08:59:52 -0400

    js update

---
+++ js/app2.js
@@ -0,0 +1,19 @@
+document.queryselector('search').addEventListener('keyup', myFuntion);
+
+function myFuntion() {
+    const searchInput = document.getElementById('search').value.toLowerCase();
+    const galleryPhoto = document.getElementsByClassName('photo');
+
+    for (let i=0; i<galleryPhoto.length; i++) {
+        let captionFilter = galleryPhoto[i].getAttribute('data-caption').toLowerCase();
+        
+        if (captionFilter.includes(searchInput)) {
+            galleryPhoto[i].style.display = '';
+        } else {
+            galleryPhoto[i].style.display = 'none';
+
+        }  
+    }
+}
+
+console.log(userSearch());
